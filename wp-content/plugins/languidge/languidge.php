fb1e07195b6c42df52e71a848ce33749b44d96ba
Kyall Barrows <kyallb@gmail.com>
2015-03-13 17:25:31 -0700

    Pull posts if it's a post, pages if it's a page

--- wp-content/plugins/languidge/languidge.php
+++ wp-content/plugins/languidge/languidge.php
@@ -13,27 +13,35 @@ function add_language_meta() {
 }
 
 function add_language_callback( $post ) {
-    wp_nonce_field( basename( __FILE__ ), 'languidge_nonce' );
-    $prfx_stored_meta = get_post_meta( $post->ID );
+    wp_nonce_field( basename( __FILE__ ), "language_nonce" );
+    $stored_meta = get_post_meta( $post->ID );
 
 	$lang = getLanguage($post->ID);
-	if ($lang == 'en'){
-		create_associated_page_selector($prfx_stored_meta, 'french', 'fr');
+	if ($lang == "fr"){
+		create_associated_page_selector($post->post_type, $stored_meta, "english", "en");
 	}
-	else if ($lang == 'fr'){
-		create_associated_page_selector($prfx_stored_meta, 'english', 'en');
+	else if ($lang == "en"){
+		create_associated_page_selector($post->post_type, $stored_meta, "french", "fr");
 	}
 	
 }
 
-function create_associated_page_selector($post_meta, $lang_name, $parent_slug){
+function create_associated_page_selector($post_type, $post_meta, $lang_name, $lang_slug){
 ?>
 	<p>
 		<label for="meta-associated-page-<?= $lang_name?>" class="prfx-row-title">Associated <?= $lang_name?> <?=get_post_type(get_the_ID())?></label>
 		<select name="meta-associated-page" id="meta-associated-page">
 		<?php
-			$parentPage = get_page_by_path($parent_slug);
-			$childPages = get_pages(array('child_of' => $parentPage->ID));
+			$childPages = array();
+			if ($post_type == 'post')
+			{
+				$childPages = get_posts(array('category_name' => $lang_slug));
+				echo "found this many posts for " . $lang_slug . "   " . sizeof($childPages);
+			}
+			else {
+				$parentPage = get_page_by_path($lang_slug);
+				$childPages = get_pages(array('child_of' => $parentPage->ID));
+			}
 			foreach ( $childPages as $childPage ) {
 				?>
 			<option value="<?php echo $childPage->ID;?>" <?php if ( isset ( $post_meta['meta-associated-page'] ) ) selected( $post_meta['meta-associated-page'][0], $childPage->ID ); ?>><?php echo $childPage->post_title ?></option>';
@@ -55,7 +63,7 @@ function language_save( $post_id ) {
     // Checks save status
     $is_autosave = wp_is_post_autosave( $post_id );
     $is_revision = wp_is_post_revision( $post_id );
-    $is_valid_nonce = ( isset( $_POST[ 'languidge_nonce' ] ) && wp_verify_nonce( $_POST[ 'languidge_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+    $is_valid_nonce = ( isset( $_POST[ "language_nonce" ] ) && wp_verify_nonce( $_POST[ "language_nonce" ], basename( __FILE__ ) ) ) ? 'true' : 'false';
  
     // Exits script depending on save status
     if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
@@ -73,6 +81,21 @@ add_action( 'add_meta_boxes', 'add_language_meta' );
 function getLanguage($postID)
 {
 	$thisPost = get_post($postID);
+	if ($thisPost->post_type == 'post')
+	{
+		$categories = wp_get_post_categories( $postID );
+		foreach ($categories as $category_id)
+		{
+			$cat = get_category( $category_id );
+			$slug = $cat->slug;
+			if ($slug === 'en' || $slug === 'fr')
+			{
+				return $cat->slug;
+			}
+		}
+		return 'no lang set';
+	}
+	
 	if ($thisPost->post_parent)
 	{
 		$parentPost = get_post($thisPost->post_parent);
