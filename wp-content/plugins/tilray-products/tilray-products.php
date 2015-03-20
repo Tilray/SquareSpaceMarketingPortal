c8c2e4bc47599b7ed7bc0285d3349bee940a8e66
Kyall Barrows <kyallb@gmail.com>
2015-03-20 15:41:37 -0700

    Add plugin that adds Tilray Products

---
+++ wp-content/plugins/tilray-products/tilray-products.php
@@ -0,0 +1,25 @@
+<?php
+ 
+/*
+Plugin Name: Tilray Products
+Description: Adds custom post type for Tilray Products
+Author: Kyall Barrows
+Version: 1.0
+*/
+
+add_action( 'init', 'create_posttype' );
+function create_posttype() {
+  register_post_type( 'tilray_product',
+    array(
+      'labels' => array(
+        'name' => __( 'Tilray Products' ),
+        'singular_name' => __( 'Tilray Product' ),
+		'new_item' => __('New Product'),
+		'search_items' => __('Search Products')
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'products'),
+    )
+  );
+}
