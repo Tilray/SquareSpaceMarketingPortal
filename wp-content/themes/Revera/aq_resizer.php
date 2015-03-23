33e8df3a87b1d02881b6705fd404a464b424bc48
Kyall Barrows <kyallb@gmail.com>
2015-03-23 15:10:38 -0700

    Ignore silly line that's breaking due to language switching

--- wp-content/themes/Revera/aq_resizer.php
+++ wp-content/themes/Revera/aq_resizer.php
@@ -32,7 +32,8 @@ function aq_resize( $url, $width, $height = null, $crop = null, $single = true )
 	$upload_url = $upload_info['baseurl'];
 	
 	//check if $img_url is local
-	if(strpos( $url, home_url() ) === false) return false;
+//	if(strpos( $url, home_url() ) === false) return false;
+	
 	
 	//define path of image
 	$rel_path = str_replace( $upload_url, '', $url);
@@ -45,7 +46,7 @@ function aq_resize( $url, $width, $height = null, $crop = null, $single = true )
 	$info = pathinfo($img_path);
 	$ext = $info['extension'];
 	list($orig_w,$orig_h) = getimagesize($img_path);
-	
+
 	//get image size after cropping
 	$dims = image_resize_dimensions($orig_w, $orig_h, $width, $height, $crop);
 	$dst_w = $dims[4];
