9a8b912fd991fedb6ffff33de0651f9449eab2b2
Kyall Barrows <kyallb@gmail.com>
2015-04-07 11:33:09 -0700

    add div.col-12's to keep things correctly aligned

--- wp-content/themes/Revera/product-page.php
+++ wp-content/themes/Revera/product-page.php
@@ -27,68 +27,72 @@ get_header(); ?>
 
 <div class="container">	
 	<div class="row filters">
-		<div class="product-filters-container">
-			<h3 class="gray-underline">Status</h3>
-			<ul class="product-filters">
-				<li><input type="radio" name="status" id="status-show-all" data-filter="" checked><label for="status-show-all">Show All</label></li>
-				<li><input type="radio" name="status" id="status-available" data-filter=".category-available" ><label for="status-available">Available</label></li>
-				<li><input type="radio" name="status" id="status-in-production" data-filter=".category-coming-soon" ><label for="status-in-production">In Production</label></li>
-			</ul>
-		</div>
-		<div class="product-filters-container">
-			<h3 class="gray-underline">Strain Type</h3>
-			<ul class="product-filters">
-				<li><input type="radio" name="strain-types" id="strain-type-show-all" data-filter="" checked><label for="strain-type-show-all">Show All</label></li>
-				<li><input type="radio" name="strain-types" id="strain-type-indica" data-filter=".category-indica" ><label for="strain-type-indica">Indica</label></li>
-				<li><input type="radio" name="strain-types" id="strain-type-sativa" data-filter=".category-sativa" ><label for="strain-type-sativa">Sativa</label></li>
-				<li><input type="radio" name="strain-types" id="strain-type-hybrid" data-filter=".category-hybrid" ><label for="strain-type-hybrid">Hybrid</label></li>
-				<li><input type="radio" name="strain-types" id="strain-type-high-cbd" data-filter=".category-high-cbd" ><label for="strain-type-high-cbd">High CBD</label></li>
-			</ul>
+		<div class="col-12">
+			<div class="product-filters-container">
+				<h3 class="gray-underline">Status</h3>
+				<ul class="product-filters">
+					<li><input type="radio" name="status" id="status-show-all" data-filter="" checked><label for="status-show-all">Show All</label></li>
+					<li><input type="radio" name="status" id="status-available" data-filter=".category-available" ><label for="status-available">Available</label></li>
+					<li><input type="radio" name="status" id="status-in-production" data-filter=".category-coming-soon" ><label for="status-in-production">In Production</label></li>
+				</ul>
+			</div>
+			<div class="product-filters-container">
+				<h3 class="gray-underline">Strain Type</h3>
+				<ul class="product-filters">
+					<li><input type="radio" name="strain-types" id="strain-type-show-all" data-filter="" checked><label for="strain-type-show-all">Show All</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-indica" data-filter=".category-indica" ><label for="strain-type-indica">Indica</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-sativa" data-filter=".category-sativa" ><label for="strain-type-sativa">Sativa</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-hybrid" data-filter=".category-hybrid" ><label for="strain-type-hybrid">Hybrid</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-high-cbd" data-filter=".category-high-cbd" ><label for="strain-type-high-cbd">High CBD</label></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<div class="row">
-	
-		<div id="primary" class="js-isotope" data-isotope-options='{ "columnWidth": 200, "itemSelector": ".product-item" }'>
-		<?php
-		$port_count = ft_of_get_option('fabthemes_port_number');
-		$port_cat =ft_of_get_option('fabthemes_portfolio');
+		<div class="col-12">
 		
-		if ( get_query_var('paged') )
-		    $paged = get_query_var('paged');
-		elseif ( get_query_var('page') )
-		    $paged = get_query_var('page');
-		else
-		    $paged = 1;
-		$wp_query = new WP_Query(array('post_type' => 'tilray_product', 'posts_per_page' => $port_count, 'paged' => $paged ));
-		?>
-		<?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
-				$all_the_tags = wp_get_post_tags( get_the_ID() );
-				$combined_tags = "";
-				foreach($all_the_tags as $this_tag){
-					$combined_tags = $combined_tags . " category-" . strtolower($this_tag->slug);
-				}
-		?>
-			<div class="col-sm-3 col-6 portbox post product-item <?= $combined_tags?>">
-					
-			 <?php
-				$thumb = get_post_thumbnail_id();
-				$img_attrs = wp_get_attachment_image_src( $thumb,'product-thumb' ); //get full URL to image (use "large" or "medium" if the images too big)
-				$image = $img_attrs[0];
-			 ?>
-						
-				<div class="hthumb">
-					<?php if($image) : ?>
-					<a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $image ?>"/></a>
-					<?php endif; ?>
-					<h3><a href="<?php the_permalink(); ?>"><p class="title"><?php the_title(); ?></p></a></h3>
-					<p><?php the_excerpt(); ?></p>
-				</div>
-
-			 
-			 </div>
+			<div id="primary" class="js-isotope" data-isotope-options='{ "columnWidth": 200, "itemSelector": ".product-item" }'>
+			<?php
+			$port_count = ft_of_get_option('fabthemes_port_number');
+			$port_cat =ft_of_get_option('fabthemes_portfolio');
 			
+			if ( get_query_var('paged') )
+				$paged = get_query_var('paged');
+			elseif ( get_query_var('page') )
+				$paged = get_query_var('page');
+			else
+				$paged = 1;
+			$wp_query = new WP_Query(array('post_type' => 'tilray_product', 'posts_per_page' => $port_count, 'paged' => $paged ));
+			?>
+			<?php while ($wp_query->have_posts()) : $wp_query->the_post(); 
+					$all_the_tags = wp_get_post_tags( get_the_ID() );
+					$combined_tags = "";
+					foreach($all_the_tags as $this_tag){
+						$combined_tags = $combined_tags . " category-" . strtolower($this_tag->slug);
+					}
+			?>
+				<div class="col-sm-3 col-6 portbox post product-item <?= $combined_tags?>">
+						
+				 <?php
+					$thumb = get_post_thumbnail_id();
+					$img_attrs = wp_get_attachment_image_src( $thumb,'product-thumb' ); //get full URL to image (use "large" or "medium" if the images too big)
+					$image = $img_attrs[0];
+				 ?>
+							
+					<div class="hthumb">
+						<?php if($image) : ?>
+						<a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $image ?>"/></a>
+						<?php endif; ?>
+						<h3><a href="<?php the_permalink(); ?>"><p class="title"><?php the_title(); ?></p></a></h3>
+						<p><?php the_excerpt(); ?></p>
+					</div>
 
-		<?php endwhile; ?>
+				 
+				 </div>
+				
+
+			<?php endwhile; ?>
+	</div>
 </div>
 
 
