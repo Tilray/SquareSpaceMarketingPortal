845538df0d01e346d96545bbbb3cf70c2ef3d049
Kyall Barrows <kyallb@gmail.com>
2015-04-21 16:34:48 -0700

    Multi-lang isn't playing well with banner posts, hard code it.

--- wp-content/themes/Revera/inc/feature.php
+++ wp-content/themes/Revera/inc/feature.php
@@ -3,8 +3,8 @@
 		<ul class="slides">
 		    <?php 	
 					$slidecat =ft_of_get_option('fabthemes_slide_categories');
-					
-					$query = new WP_Query( array( 'cat' =>$slidecat,'posts_per_page' =>'100' ) );
+
+					$query = new WP_Query( array( 'category_name' => 'banners,banners-fr','posts_per_page' =>'100' ) );
 		           	if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();	?>
 		 	
 			 		<li>
