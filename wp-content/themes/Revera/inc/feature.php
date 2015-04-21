ce3894c63a64d7e321a8dc26a19912d608eeb426
Kyall Barrows <kyallb@gmail.com>
2015-04-21 16:49:54 -0700

    Trying to fix banners

--- wp-content/themes/Revera/inc/feature.php
+++ wp-content/themes/Revera/inc/feature.php
@@ -4,7 +4,7 @@
 		    <?php 	
 					$slidecat =ft_of_get_option('fabthemes_slide_categories');
 
-					$query = new WP_Query( array( 'category_name' => 'banners,banners-fr','posts_per_page' =>'100' ) );
+					$query = new WP_Query( array( 'category_name' => 'banners,banners-fr,bannersfr','posts_per_page' =>'100' ) );
 		           	if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();	?>
 		 	
 			 		<li>
