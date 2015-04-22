b92fc5c1c75904571f20ff945f365cc07bf53aea
Kyall Barrows <kyallb@gmail.com>
2015-04-22 15:44:23 -0700

    Remove french category tweak

--- wp-content/themes/Revera/product-page.php
+++ wp-content/themes/Revera/product-page.php
@@ -28,13 +28,6 @@ get_header(); ?>
 
 
 <?php
-	$filterLangCode = "";
-	$currLangCode = get_current_language_code();
-	if ($currLangCode != "en")
-	{
-		$filterLangCode	= "-" . $currLangCode;
-	}
-
 	//get incoming status and straintype params
 	$qpStrain = 'straintype';
 	$qpStatus = 'status';
@@ -67,18 +60,18 @@ get_header(); ?>
 				<h3 class="gray-underline">Status</h3>
 				<ul class="product-filters">
 					<li><input type="radio" name="status" id="status-show-all" data-filter="" checked><label for="status-show-all">Show All</label></li>
-					<li><input type="radio" name="status" id="status-available" data-filter=".category-available<?=$filterLangCode?>" ><label for="status-available">Available</label></li>
-					<li><input type="radio" name="status" id="status-in-production" data-filter=".category-in-production<?=$filterLangCode?>" ><label for="status-in-production">In Production</label></li>
+					<li><input type="radio" name="status" id="status-available" data-filter=".category-available" ><label for="status-available">Available</label></li>
+					<li><input type="radio" name="status" id="status-in-production" data-filter=".category-in-production" ><label for="status-in-production">In Production</label></li>
 				</ul>
 			</div>
 			<div class="product-filters-container">
 				<h3 class="gray-underline">Strain Type</h3>
 				<ul class="product-filters">
 					<li><input type="radio" name="strain-types" id="strain-type-show-all" data-filter="" checked><label for="strain-type-show-all">Show All</label></li>
-					<li><input type="radio" name="strain-types" id="strain-type-indica" data-filter=".category-indica<?=$filterLangCode?>" ><label for="strain-type-indica">Indica</label></li>
-					<li><input type="radio" name="strain-types" id="strain-type-sativa" data-filter=".category-sativa<?=$filterLangCode?>" ><label for="strain-type-sativa">Sativa</label></li>
-					<li><input type="radio" name="strain-types" id="strain-type-hybrid" data-filter=".category-hybrid<?=$filterLangCode?>" ><label for="strain-type-hybrid">Hybrid</label></li>
-					<li><input type="radio" name="strain-types" id="strain-type-high-cbd" data-filter=".category-high-cbd<?=$filterLangCode?>" ><label for="strain-type-high-cbd">High CBD</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-indica" data-filter=".category-indica" ><label for="strain-type-indica">Indica</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-sativa" data-filter=".category-sativa" ><label for="strain-type-sativa">Sativa</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-hybrid" data-filter=".category-hybrid" ><label for="strain-type-hybrid">Hybrid</label></li>
+					<li><input type="radio" name="strain-types" id="strain-type-high-cbd" data-filter=".category-high-cbd" ><label for="strain-type-high-cbd">High CBD</label></li>
 				</ul>
 			</div>
 		</div>
@@ -141,11 +134,11 @@ get_header(); ?>
 	$combinedFilter = "";
 	if ($status != "")
 	{
-		$combinedFilter = $combinedFilter . ".category-" . $status . $filterLangCode;
+		$combinedFilter = $combinedFilter . ".category-" . $status;
 	}
 	if ($strainType != "")
 	{
-		$combinedFilter = $combinedFilter . ".category-" . $strainType . $filterLangCode;
+		$combinedFilter = $combinedFilter . ".category-" . $strainType;
 	}
 	
 	if ($combinedFilter == "")
@@ -176,10 +169,10 @@ get_header(); ?>
 		jQuery('#primary').isotope({ filter: preFilter });
 		if (preselectedStatus != "")
 		{
-			jQuery('input[data-filter=".category-' + preselectedStatus + '<?= $filterLangCode ?>"]').prop('checked', true);
+			jQuery('input[data-filter=".category-' + preselectedStatus + '"]').prop('checked', true);
 		}
 		if (preselectedStrainType != ""){
-			jQuery('input[data-filter=".category-' + preselectedStrainType + '<?= $filterLangCode ?>"]').prop('checked', true);
+			jQuery('input[data-filter=".category-' + preselectedStrainType + '"]').prop('checked', true);
 		}
 
 		jQuery('ul.product-filters input[type=radio]').change(function() {
