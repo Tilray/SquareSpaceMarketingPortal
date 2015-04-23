0f687d5e0b350f64e3c5167ef816022ddf69dc9a
Kyall Barrows <kyallb@gmail.com>
2015-04-23 11:50:34 -0700

    SIngle blog page styling

--- wp-content/themes/Revera/single.php
+++ wp-content/themes/Revera/single.php
@@ -11,15 +11,8 @@ get_header(); ?>
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<?php 
-				$port_cat =ft_of_get_option('fabthemes_portfolio');
-				if (in_category($port_cat)) { ?>
-				<h1> Portfolio item </h1> 
-				<p> Project item from your portfolio</p>
-				<?php } else { ?>
-				<h1> Blog post </h1> 
-				<p> Article from your blog</p>
-				<?php } ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<h1><?php the_title() ?></h1>
 			</div>
 			
 		</div>
@@ -31,22 +24,13 @@ get_header(); ?>
 	<div id="primary" class="content-area col-sm-8">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'content', 'single' ); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
-			?>
 
 		<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 	</div>
 </div>
 <?php get_footer(); ?>
