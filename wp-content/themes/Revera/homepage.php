9300c665bafcbbc9c7a48743127ed75845c7d7c0
Kyall Barrows <kyallb@gmail.com>
2015-04-24 10:43:23 -0700

    Switch 3-6-3 layout to a 12 on homepage

--- wp-content/themes/Revera/homepage.php
+++ wp-content/themes/Revera/homepage.php
@@ -23,11 +23,9 @@ get_header(); ?>
 
 <div class="section-wide">
 	<div class="row">
-		<div class="col-3"></div>
-		<div class="col-6 homepage-main-content">
+		<div class="col-12 homepage-main-content">
 			<?php echo $postContent; ?>
 		</div>
-		<div class="col-3"></div>
 	</div>
 </div>
 
