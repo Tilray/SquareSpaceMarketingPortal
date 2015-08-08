a04418210b003064f5ceb956606236d991c7ca01
Kyall Barrows <kyallb@gmail.com>
2015-08-07 18:57:42 -0700

    Add function for sitemap generation

--- wp-content/themes/Revera/functions.php
+++ wp-content/themes/Revera/functions.php
@@ -325,3 +325,28 @@ function blogroot_func( $atts ){
 	return get_site_url();
 }
 add_shortcode( 'blogroot', 'blogroot_func' );
+
+add_action( "save_post", "eg_create_sitemap" );   
+function eg_create_sitemap() {
+    $postsForSitemap = get_posts( array(
+        'numberposts' => -1,
+        'orderby'     => 'modified',
+        'post_type'   => array( 'post', 'page' ),
+        'order'       => 'DESC'
+    ) );
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
+    $sitemap .= "\n" . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";    
+    foreach( $postsForSitemap as $post ) {
+        setup_postdata( $post );   
+        $postdate = explode( " ", $post->post_modified );   
+        $sitemap .= "\t" . '<url>' . "\n" .
+            "\t\t" . '<loc>' . get_permalink( $post->ID ) . '</loc>' .
+            "\n\t\t" . '<lastmod>' . $postdate[0] . '</lastmod>' .
+            "\n\t\t" . '<changefreq>weekly</changefreq>' .
+            "\n\t" . '</url>' . "\n";
+    }     
+    $sitemap .= '</urlset>';     
+    $fp = fopen( ABSPATH . "sitemap.xml", 'w' );
+    fwrite( $fp, $sitemap );
+    fclose( $fp );
+}
