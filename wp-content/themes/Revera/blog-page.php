e149efd4c80458cd189478e90f71aeba21eddd99
Kyall Barrows <kyallb@gmail.com>
2015-09-14 14:04:32 -0700

    Make news page show 60 posts.  Stopgap measure until we can ship pagination

--- wp-content/themes/Revera/blog-page.php
+++ wp-content/themes/Revera/blog-page.php
@@ -30,7 +30,7 @@ get_header(); ?>
 			$catName = 'blog-' . get_current_language_code();
 			$args = array(
 				'category_name' => $catName,
-				'posts_per_page' => '12'
+				'posts_per_page' => '60'
 			);
 			render_news_section($args, true, true);
 			?>
