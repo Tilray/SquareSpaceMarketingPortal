8fa268e2cf18bad2c7477f3325c78541d1fc874b
Kyall Barrows <kyall@pixelheavyindustries.com>
2016-02-24 10:32:58 -0800

    WIP on mobile footer

--- wp-content/themes/Revera/footer.php
+++ wp-content/themes/Revera/footer.php
@@ -9,16 +9,27 @@
 ?>
 
 	</div><!-- #content -->
-<footer id="colophon" class="site-footer" role="contentinfo">
+<footer class="site-footer mobile" role="contentinfo">
 	<div class="container">
 		<div class="site-info wrap row">
 			<div id="copyright-footer-container" class="fcred col-12">
 				&copy; <?=date("Y")?> <?php _e('Tilray, all rights reserved'); ?> <?php wp_nav_menu( array( 'container' => '', 'theme_location' => 'copyright-footer','container_class' => 'copyright-footer','menu_id'=>'copyright-footer-menu' ,'menu_class'=>'copyright-footer' ) ); ?>
 			</div>		
 
-		</div><!-- .site-info -->
+		</div>
 	</div>	
-</footer><!-- #colophon .site-footer -->
+</footer>
+
+<footer id="colophon" class="site-footer desktop" role="contentinfo">
+	<div class="container">
+		<div class="site-info wrap row">
+			<div id="copyright-footer-container" class="fcred col-12">
+				&copy; <?=date("Y")?> <?php _e('Tilray, all rights reserved'); ?> <?php wp_nav_menu( array( 'container' => '', 'theme_location' => 'copyright-footer','container_class' => 'copyright-footer','menu_id'=>'copyright-footer-menu' ,'menu_class'=>'copyright-footer' ) ); ?>
+			</div>		
+
+		</div>
+	</div>	
+</footer>
 
 
 <?php wp_footer(); ?>
