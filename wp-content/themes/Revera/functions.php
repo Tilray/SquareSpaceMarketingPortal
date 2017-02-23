67e886126a7405a21e9722b7072a63c8e2e6b84d
Kyall Barrows <kyall@pixelheavyindustries.com>
2017-02-22 16:17:41 -0800

    Start collecting post view counts, for use in popular posts metrics

--- wp-content/themes/Revera/functions.php
+++ wp-content/themes/Revera/functions.php
@@ -1008,3 +1008,20 @@ if( function_exists('acf_add_options_page') ) {
 //totally do not need emojis on this site.  Let's shave off 5k and 3 file loads!
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+//for collecting most popular posts data
+function wpb_set_post_views($postID) {
+    $count_key = 'wpb_post_views_count';
+    $count = get_post_meta($postID, $count_key, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $count_key, $count);
+    }
+}
+//To keep the count accurate, lets get rid of prefetching
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
