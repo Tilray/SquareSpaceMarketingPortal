58964d0b5620f4e2569a65fd11aacae83da4bf59
Kyall Barrows <kyall@pixelheavyindustries.com>
2017-05-30 12:13:24 -0700

    New homepage, initial pass

--- wp-content/themes/Revera/homepage.php
+++ wp-content/themes/Revera/homepage.php
@@ -19,23 +19,35 @@ get_header(); ?>
 ?>
 
 <?php
-	if ($isMobile){
-		?>
-		<div class="home-shop-now-container">
-			<a href="https://store.tilray.ca">
-				<div class="home-register-button">
-					<h2><?= _("Shop Now")?></h2>
-				</div>
-			</a>
-		</div>
-		<?php
-	}
-	else
-	{
-		get_template_part( 'inc/feature' );
-	}
+	$header_url = wp_get_attachment_image_src(get_field("header_image", get_the_ID()), "full");
+	$header_link = get_field("header_link", get_the_ID());
 ?>
+<div class="homepage-header-container" style="background-image:url(<?=$header_url[0]?>)">
+	<a href="<?=$header_link?>"></a>
+</div>
 <div class="container ">
+<div class="row">
+	<div class="col-sm-1 hidden-xs"></div>
+	<div class="col-sm-10 col-xs-12">
+	<?php
+	if( have_rows('point_sections', $post->ID) ){
+		while( have_rows('point_sections', $post->ID)){
+			the_row();
+			$img_attrs = wp_get_attachment_image_src( get_sub_field('image'), 'thumbnail' ); 
+			$image = $img_attrs[0];
+			?>
+			<div class="col-sm-4 col-xs-12 homepage-point">
+				<div class="round-image" style="background-image:url(<?= $image ?>)"><a href="<?= get_sub_field('link') ?>"></a></div>
+				<a href="<?= get_sub_field('link') ?>"><h2><?=get_sub_field('title')?></h2></a>
+				<?=get_sub_field('content')?>
+			</div>
+			<?php
+		}
+	}
+	?>
+	</div>
+	<div class="col-sm-1 hidden-xs"></div>
+</div>
 
 <?php
 	$langCode = get_current_language_code();
@@ -47,10 +59,35 @@ get_header(); ?>
 ?>
 <div class="section-wide">
 	<div class="row">
-		<div class="col-lg-12 homepage-main-content">
-			<?php echo $postContent; ?>
+		<div class="homepage-blog-container">
+			<?php
+			$count = 1;
+			if( have_rows('feature_sections', $post->ID) ){
+				while( have_rows('feature_sections', $post->ID)){
+					the_row();
+					$left = $count % 2 == 0;
+					$img_attrs = wp_get_attachment_image_src( get_sub_field('image'), 'large' ); 
+					$image = $img_attrs[0];
+					?>
+						<div class="homepage-feature-section <?= $left ? "left" : "right" ?>">
+							<?php if ($left){?>
+								<div class="image"><img src="<?= $image ?>"></div>
+							<?php } ?>
+							<div class="content"><?=get_sub_field('content')?></div>
+							<?php if (!$left){?>
+								<div class="image"><img src="<?= $image ?>"></div>
+							<?php } ?>
+						</div>
+					<?php
+
+					$count++;
+				}
+			}
+			?>
 		</div>
 	</div>
+</div>
+<div class="section-wide">
 	<div class="row">
 	<div class="boxitems col-lg-12 homepage-blog-container">
 		<a href="/<?=strtolower($langCode)?>/<?=$catName?>">
@@ -60,14 +97,11 @@ get_header(); ?>
 		if( have_rows('homepage_news_posts', 'options') ){
 			while( have_rows('homepage_news_posts', 'options')){
 				the_row();
-
-				$post_id = get_sub_field('news_posts');
 				?>
 				<div class="blog-post-preview col-sm-4">
 					<?php
-					$thumbID = get_post_thumbnail_id($post_id);
-					$img_attrs = wp_get_attachment_image_src( $thumbID,'blog-preview' ); 
-					$image = $img_attrs[0];
+					$post_id = get_sub_field('news_posts');
+					$image = get_image_url_from_image_id(get_sub_field('news_posts'), 'blog-preview');
 					if($image) {?>
 						<a href="<?php the_permalink($post_id); ?>" class="prevent-reflow">
 							<img class="blog-preview" src="<?= $image ?>" alt="<?= get_the_title($post_id); ?>"/>
