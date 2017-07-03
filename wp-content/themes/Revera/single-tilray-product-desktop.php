e2d7e2aa8b3c027fb2012f6418f7b4ddd0cdacfb
Kyall Barrows <kyall@pixelheavyindustries.com>
2017-07-03 14:29:49 -0700

    Comment out notifications

--- wp-content/themes/Revera/single-tilray-product-desktop.php
+++ wp-content/themes/Revera/single-tilray-product-desktop.php
@@ -81,13 +81,13 @@ $prev_page_link = $is_accessories_page ? getAccessoriesPageLink() : getProductsP
 						else if ($thisProduct->status == '90-days')
 							echo __("Available within 90 days.");
 
-						$notification_url = "/" . get_current_language_code() . "/";
+/*						$notification_url = "/" . get_current_language_code() . "/";
 						if (strtolower(get_current_language_code()) == "fr")
 							$notification_url .= "le-notification-terms";
 						else
 							$notification_url .= "product-alerts-terms-of-service";
 						$notification_url .= "/?product_id=" . $post->ID;
-						echo "</h4><p><a href='$notification_url'>" . __("Notify me when this strain is available") . " &raquo;</a></p>";
+						echo "</h4><p><a href='$notification_url'>" . __("Notify me when this strain is available") . " &raquo;</a></p>";*/
 					}
 					?>
 				</p>
